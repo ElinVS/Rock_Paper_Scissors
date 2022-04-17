3b9c6659f2187e6a6d2a65ffe66cbccf76218c84
ElinVS <elinvsvennberg@gmail.com>
2022-04-17 12:56:04 +0100

    so close

--- models/game.py
+++ models/game.py
@@ -30,34 +30,22 @@ def play_the_game(player1,player2):
 
 def computers_choice():
     rps =["rock", "paper", "scissors"]
-    return rps[random.randrange(0,3)]
+    return rps[random.randint(0,2)]
+
 
 def player_vs_computer(player, computer):
-
-    winner = computer
+    winner = "computer"
 
     if player == computer:
         winner = "draw"
-
-    if player == "rock" and computer == "paper":
-        winner = computer
-
-    if player == "paper" and computer == "scissors":
-        winner = computer
-
-    if player == "scissors" and computer == "rock":
-        winner = computer
-
     if player == "rock" and computer == "scissors":
-        winner = player
-    
-    if player == "paper" and computer == "rock":
-        winner = player
-    
+        winner = "player"
     if player == "scissors" and computer == "paper":
-        winner = player
+        winner = "player"
+    if player == "paper" and computer == "rock":
+        winner = "player"
 
-        return winner
+    return winner
 
 
 
